90386766992b5b1270bffad59a890eb0756c750a
mehrdad29 <mehrdad29@gmail.com>
2014-04-25 19:27:34 +0430

    My Code!

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## my functions caching the inverse of a matrix rather than compute it repeatedly.
 
-## Write a short comment describing this function
+## it takes an argument x of type matrix and it returns a list 
+## with 4 list items (they are actually 4 functions wrapped in a list)
+## if the inverse is not computed yet then i will be null and otherwise
+## i is the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-  i <- NULL
-  set <- function(y) {
+  i <- NULL                                       # when makeCacheMatrix just called set i to Null        
+  set <- function(y) {                            # this function set matrix
     x <<- y
-    i <<- NULL
+    i <<- NULL                                    # i set to Null because matrix is just set and its inverse is not computed
   }
-  get <- function() x
-  setinversae <- function(inverse) i <<- inverse
-  getinverse <- function() i
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+  get <- function() x                             # this function get matrix
+  setInverse <- function(inverse) i <<- inverse  # this function set inverse of matrix
+  getInverse <- function() i                      # this function get inverse of matrix
+  list(set = set, get = get,                      # return the list that contains four functions
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## the input is expecting a "special vector" made from makeCacheMatrix.
+## the output is the inverse of the matrix coming whether from the special vector's 
+## cache or computation.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  i <- x$getinverse()               #query the x vector's cache
-  if(!is.null(i)) {                 #if there is a cache
+  i <- x$getInverse()               # query the x vector's cache
+  if(!is.null(i)) {                 # if there is a cache
     message("getting cached data")  
-    return(i)                       #just return the cache, no computation needed
+    return(i)                       # just return the cache, no computation needed
   }
-  data <- x$get()                   #if there's no cache
-  i <- solve(data, ...)             #we actually compute them here
-  x$setinverse(i)                   #save the result back to x's cache
-  i                                 #return the result
+  data <- x$get()                   # if there's no cache
+  i <- solve(data, ...)             # we actually compute them here
+  x$setInverse(i)                   # save the result back to x's cache
+  i                                 # return the result
 }
+
